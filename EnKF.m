27dde42afd01c65a768747bf809ed7e70fd14dbe
Greg Stroot <gstroot@caltech.edu>
2021-11-10 09:26:11 -0800

    Added cyl_enkf to main folder. NOTE: We must add the working directory to be femmat-p/sandbox for this to function

---
+++ EnKF.m
@@ -0,0 +1,32 @@
+function [one, two, three] = EnKF(x)
+  %{
+  Summary:  Runs Ensemble Kalman Filter following approach from TODO where?
+
+    Takes in pressure measurements and POD coordinates
+    TODO What should this output?
+  
+  
+  Inputs:
+    Input1 - Type:
+            -
+    Input2 - Type:
+            -
+    Input3 - Type:
+            -
+  
+  Outputs:
+  
+    Output1 - Type:
+            -
+    Output2 - Type:
+            -
+    Output3 - Type:
+            -
+  %}
+
+
+
+
+
+
+end
---
+++ main.m
@@ -0,0 +1,4 @@
+%Import files to run propreitary solver
+addpath('femmat-p/sandbox')
+
+
